f14325bccb790f7f777aa04f9c4b219eeb9272e1
patrickpcodes <patrickpetropoulos@protonmail.com>
2024-09-08 16:46:00 -0400

    Lesson 5

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 from constants import *  
+from player import Player
+
+FPS = 60
 
 
 def main():
@@ -8,13 +11,19 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+   
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((1,1,1))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(FPS)/1000        
 
 
 if __name__ == "__main__":
